af87d0b0d7eed79271ed32c9adebe9e5451065cf
xxxjahlilallenxxx <jahlilallen@gmail.com>
2022-04-28 13:00:32 -0400

    Updated Testing

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,32 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+    @Test
+    public void RunMainTest(){
+        Assertions.assertEquals(isRobotAtEnd(9,9,9,9), isDone());
+        SpaceLimit spaceLimit = SpaceLimit.OFF_LIMITS;
+        SpaceLimit emptyBlock = SpaceLimit.FREE;
+        Assertions.assertEquals(spaceLimit, SpaceLimit.OFF_LIMITS);
+        Assertions.assertNotEquals(spaceLimit, emptyBlock);
+    }
+
+    public boolean isDone(){
+        return true;
+    }
+
+    public boolean isNotDone(){
+        return false;
+    }
+
+    public enum SpaceLimit {OFF_LIMITS, FREE}
+
+    public boolean isRobotAtEnd(int maxRow, int maxCol, int currRow, int currCol){
+        if ((maxRow == currRow) && (maxCol == currCol)){
+            return isDone();
+        }
+        else {
+            return isNotDone();
+        }
+    }
 }
